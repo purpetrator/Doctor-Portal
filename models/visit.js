5408ab35397bc92df9d2473e0e09f0c1f910489e
Ana Chernov <anastasia.chernov@gmail.com>
2019-12-05 20:40:18 -0500

    Created visit history functionality with dynamically populated modal

--- models/visit.js
+++ models/visit.js
@@ -1,25 +1,27 @@
 module.exports = function(sequelize, DataTypes) {
   var Visit = sequelize.define("Visit", {
     date: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1]
-      }
+      type: DataTypes.STRING
     },
     symptoms: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-      validate: {
-        len: [1]
-      }
+      type: DataTypes.TEXT
     },
     rx: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1]
-      }
+      type: DataTypes.STRING
+    },
+    type: {
+      type: DataTypes.STRING
+    },
+    pid: {
+      type: DataTypes.STRING
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: sequelize.literal("CURRENT_TIMESTAMP()")
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      defaultValue: sequelize.literal("CURRENT_TIMESTAMP()")
     }
   });
   return Visit;
--- public/js/index.js
+++ public/js/index.js
@@ -42,6 +42,12 @@ var API = {
       type: "PUT",
       data: updatedPatient
     });
+  },
+  getVisit: function(pid) {
+    return $.ajax({
+      url: "api/visits/" + pid,
+      type: "GET"
+    });
   }
 };
 
@@ -138,14 +144,142 @@ var handleUpdateBtnClick = function() {
   };
 
   API.updatePatient(idToUpdate, updatedPatient).then(function() {
-    $("#info-modal").modal("hide");
+    $("#patient-modal").modal("hide");
     alert("Successfully updated");
   });
+};
+
+var handleVisitBtnClick = function() {
+  event.preventDefault();
+  console.log("you clicked");
+  console.log("-----------");
+
+  var patientID = $("#patient-id").val();
+
+  console.log("patientID: " + patientID);
+
+  API.getVisit(patientID).then(function(dbVisit) {
+    $("#patient-modal").modal("hide");
+    $("#visit-body").empty();
+    // console.log(dbVisit);
+    for (var i = 0; i < dbVisit.length; i++) {
+      // console.log(dbVisit[i]);
+
+      var date = dbVisit[i].date;
+      var rx = dbVisit[i].rx;
+      var symptoms = dbVisit[i].symptoms;
+      var type = dbVisit[i].type;
+
+      var card = $("<div>");
+      card.addClass("card border-info mb-3 form-rounded m-3 width");
+
+      var cardHeader = $("<div>");
+      cardHeader.addClass("card-header form-rounded");
+      cardHeader.text(date);
+      cardHeader.append(" <i class='fas fa-calendar'></i>");
+
+      var cardBody = $("<div>");
+      cardBody.addClass("card-body");
+      cardBody.attr("id", "visit-card");
+
+      var title = $("<h5>");
+      title.text("Visit Type: " + type);
+      title.addClass("visit-type");
+
+      var paragraph1 = $("<p>");
+      paragraph1.text("Symptoms: " + symptoms);
+      paragraph1.addClass("card-text");
+
+      var paragraph2 = $("<p>");
+      paragraph2.text("Medications Prescribed: " + rx);
+      paragraph2.addClass("card-text");
+
+      cardBody.append(title);
+      cardBody.append(paragraph1);
+      cardBody.append(paragraph2);
+      card.append(cardHeader);
+      card.append(cardBody);
+
+      $("#visit-body").append(card);
+    }
+  });
+
+  // for loop over dbVisit and dynamically create card to display each item in array
+
+  //   $("#patient-info").empty();
+  //   $("#patient-info").append(
+  //     "<span>" +
+  //       "<strong>" +
+  //       "Name: " +
+  //       "</strong>" +
+  //       feedback.selection.value.lastname +
+  //       " " +
+  //       feedback.selection.value.firstname +
+  //       "</span>" +
+  //       "<br>" +
+  //       "<span>" +
+  //       "<strong>" +
+  //       "Date of Birth: " +
+  //       "</strong>" +
+  //       feedback.selection.value.dob +
+  //       "</span>" +
+  //       "<br>" +
+  //       "<span>" +
+  //       "<strong>" +
+  //       "Address: " +
+  //       "</strong>" +
+  //       feedback.selection.value.street +
+  //       ", " +
+  //       feedback.selection.value.city +
+  //       ", " +
+  //       feedback.selection.value.state +
+  //       ", " +
+  //       feedback.selection.value.zip +
+  //       "</span>" +
+  //       "<br>" +
+  //       "<span>" +
+  //       "<strong>" +
+  //       "Phone: " +
+  //       "</strong>" +
+  //       feedback.selection.value.phone +
+  //       "</span>" +
+  //       "<br>" +
+  //       "<br>" +
+  //       "<span>" +
+  //       "<strong>" +
+  //       "Height: " +
+  //       "</strong>" +
+  //       feedback.selection.value.height +
+  //       " in." +
+  //       "</span>" +
+  //       " " +
+  //       "<span>" +
+  //       "<strong>" +
+  //       "Weight: " +
+  //       "</strong>" +
+  //       feedback.selection.value.weight +
+  //       " lbn." +
+  //       "</span>" +
+  //       "<br>"
+  //   );
+  //   $("#patient-info").append(
+  //     "<input type='hidden' id='patient-id' value='" +
+  //       feedback.selection.value.id +
+  //       "'>"
+  //   );
+};
+
+var closeVisit = function() {
+  $("#patient-modal").modal("show");
 };
 
 // Add event listeners to the submit and delete buttons
 $submitBtn.on("click", handleFormSubmit);
 
-$("#delete-btn").on("click", handleDeleteBtnClick);
+$(document).on("click", "#delete-btn", handleDeleteBtnClick);
 
-$("#update-btn").on("click", handleUpdateBtnClick);
+$(document).on("click", "#update-btn", handleUpdateBtnClick);
+
+$(document).on("click", "#visit-btn", handleVisitBtnClick);
+
+$(document).on("click", "#visit-close", closeVisit);
