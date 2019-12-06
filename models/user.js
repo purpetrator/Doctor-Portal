70456be93b651d010bdaa5387df86af0e4bb31b5
Ana Chernov <anastasia.chernov@gmail.com>
2019-12-06 12:28:56 -0500

    Drying out code

--- models/user.js
+++ models/user.js
@@ -42,13 +42,5 @@ module.exports = function(sequelize, Sequelize) {
     }
   });
 
-  // User.associate = function(models) {
-  //   // Associating User with Posts
-  //   // When an User is deleted, also delete any associated Posts
-  //   User.hasMany(models.Patient, {
-  //     onDelete: "cascade"
-  //   });
-  // };
-
   return User;
 };
